8ded7f6f3a8e60ab583f11531c0d9dd59982dff1
Daniel Zaoui <daniel.zaoui@yahoo.com>
2018-12-24 13:39:19 +0200

    Use read instead of getline

--- zplay.c
+++ zplay.c
@@ -26,45 +26,50 @@ _on_stdin(void *data, Ecore_Fd_Handler *fdh EINA_UNUSED)
 {
    static Eina_Bool _progress = EINA_FALSE;
    Eo *emo = data;
-   char *line = NULL;
-   size_t len = 0;
-   ssize_t r = getline(&line, &len, stdin);
+   char *line = alloca(1024);
+   ssize_t nb = read(STDIN_FILENO, line, 1024);
 
-   if (r < 0)
+   while (nb > 0)
+     {
+        char *eol = strchr(line, '\n');
+        if (eol) *eol = '\0';
+        int len = strlen(line);
+        if (!strcmp(line, "PAUSE")) emotion_object_play_set(emo, EINA_FALSE);
+        else if (!strcmp(line, "PLAY")) emotion_object_play_set(emo, EINA_TRUE);
+        else if (!strcmp(line, "STOP"))
+          {
+             emotion_object_play_set(emo, EINA_FALSE);
+             emotion_object_position_set(emo, 0.0);
+          }
+        else if (!strcmp(line, "SHOW_PROGRESS"))
+          {
+             if (!_progress)
+               {
+                  efl_event_callback_add
+                     (emo, EFL_CANVAS_VIDEO_EVENT_POSITION_CHANGE, _media_position_update, NULL);
+                  _progress = EINA_TRUE;
+               }
+          }
+        else if (!strcmp(line, "HIDE_PROGRESS"))
+          {
+             efl_event_callback_del
+                (emo, EFL_CANVAS_VIDEO_EVENT_POSITION_CHANGE, _media_position_update, NULL);
+             _progress = EINA_FALSE;
+          }
+        else if (!strncmp(line, "FILE ", 5))
+          {
+             emotion_object_file_set(emo, line + 5);
+          }
+        line += len + 1;
+        nb -= (len + 1);
+        if (nb < 0) nb = 0;
+     }
+   if (nb < 0)
      {
         fprintf(stderr, "ERROR: could not read from stdin: %s\n", strerror(errno));
         elm_exit();
         return ECORE_CALLBACK_CANCEL;
      }
-   if (!strcmp(line, "PAUSE\n")) emotion_object_play_set(emo, EINA_FALSE);
-   else if (!strcmp(line, "PLAY\n")) emotion_object_play_set(emo, EINA_TRUE);
-   else if (!strcmp(line, "STOP\n"))
-     {
-        emotion_object_play_set(emo, EINA_FALSE);
-        emotion_object_position_set(emo, 0.0);
-     }
-   else if (!strcmp(line, "SHOW_PROGRESS\n"))
-     {
-        if (!_progress)
-          {
-             efl_event_callback_add
-                (emo, EFL_CANVAS_VIDEO_EVENT_POSITION_CHANGE, _media_position_update, NULL);
-             _progress = EINA_TRUE;
-          }
-     }
-   else if (!strcmp(line, "HIDE_PROGRESS\n"))
-     {
-        efl_event_callback_del
-           (emo, EFL_CANVAS_VIDEO_EVENT_POSITION_CHANGE, _media_position_update, NULL);
-        _progress = EINA_FALSE;
-     }
-   else if (!strncmp(line, "FILE ", 5))
-     {
-        char *eol = strchr(line, '\n');
-        *eol = '\0';
-        emotion_object_file_set(emo, line + 5);
-     }
-   free(line);
    return ECORE_CALLBACK_RENEW;
 }
 
