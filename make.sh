08bd341f504f337098ffce7e4a46291f4678ab2a
Daniel Zaoui <daniel.zaoui@yahoo.com>
2019-01-19 20:56:45 +0200

    Update installation path

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 gcc -g zplay.c $CFLAGS `pkg-config --cflags --libs elementary` -o zplay
 [ $? -eq 0 ] || exit 1
-sudo /usr/bin/install -c zplay /opt/e/bin/
+sudo /usr/bin/install -c zplay /opt/mine/bin/
