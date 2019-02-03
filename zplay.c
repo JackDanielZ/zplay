0c95a135f02a9fbd4c2be65dba72f2d119834290
Daniel Zaoui <daniel.zaoui@yahoo.com>
2019-02-03 21:35:46 +0200

    Globalize emo to permit access from other flows

--- zplay.c
+++ zplay.c
@@ -10,6 +10,8 @@ typedef struct
    Eina_Stringshare *path;
 } Song_Info;
 
+static Eo *_emo = NULL;
+
 static Eina_List *_queue = NULL, *_cur_queue_item = NULL;
 static Eina_Bool _pause_on_notfound = EINA_FALSE;
 
@@ -29,14 +31,14 @@ _media_finished(void *data EINA_UNUSED, const Efl_Event *ev)
 }
 
 static void
-_item_play(Eo *emo, Song_Info *info)
+_item_play(Song_Info *info)
 {
    if (ecore_file_exists(info->path))
      {
-        emotion_object_play_set(emo, EINA_FALSE);
-        emotion_object_position_set(emo, 0.0);
-        emotion_object_file_set(emo, info->path);
-        emotion_object_play_set(emo, EINA_TRUE);
+        emotion_object_play_set(_emo, EINA_FALSE);
+        emotion_object_position_set(_emo, 0.0);
+        emotion_object_file_set(_emo, info->path);
+        emotion_object_play_set(_emo, EINA_TRUE);
      }
    else printf("FILE_NOT_FOUND: %s\n", info->id);
 }
@@ -58,10 +60,9 @@ end:
 }
 
 static Eina_Bool
-_on_stdin(void *data, Ecore_Fd_Handler *fdh EINA_UNUSED)
+_on_stdin(void *data EINA_UNUSED, Ecore_Fd_Handler *fdh EINA_UNUSED)
 {
    static Eina_Bool _progress = EINA_FALSE;
-   Eo *emo = data;
    char *line = alloca(1024);
    ssize_t nb = read(STDIN_FILENO, line, 1024);
 
@@ -70,7 +71,7 @@ _on_stdin(void *data, Ecore_Fd_Handler *fdh EINA_UNUSED)
         char *eol = strchr(line, '\n');
         if (eol) *eol = '\0';
         int len = strlen(line);
-        if (!strcmp(line, "PAUSE")) emotion_object_play_set(emo, EINA_FALSE);
+        if (!strcmp(line, "PAUSE")) emotion_object_play_set(_emo, EINA_FALSE);
         else if (!strncmp(line, "PLAY", 4))
           {
              if (line[4] == ' ')
@@ -79,43 +80,43 @@ _on_stdin(void *data, Ecore_Fd_Handler *fdh EINA_UNUSED)
                   if (info)
                     {
                        _cur_queue_item = eina_list_data_find_list(_queue, info);
-                       _item_play(emo, info);
+                       _item_play(info);
                     }
                }
-             else emotion_object_play_set(emo, EINA_TRUE);
+             else emotion_object_play_set(_emo, EINA_TRUE);
           }
         else if (!strcmp(line, "STOP"))
           {
-             emotion_object_play_set(emo, EINA_FALSE);
-             emotion_object_position_set(emo, 0.0);
+             emotion_object_play_set(_emo, EINA_FALSE);
+             emotion_object_position_set(_emo, 0.0);
           }
         else if (!strcmp(line, "NEXT"))
           {
              Eina_List *next = eina_list_next(_cur_queue_item);
              if (!next) next = _queue;
              _cur_queue_item = next;
-             _item_play(emo, eina_list_data_get(next));
+             _item_play(eina_list_data_get(next));
           }
         else if (!strcmp(line, "PREV"))
           {
              Eina_List *prev = eina_list_prev(_cur_queue_item);
              if (!prev) prev = eina_list_last(_queue);
              _cur_queue_item = prev;
-             _item_play(emo, eina_list_data_get(prev));
+             _item_play(eina_list_data_get(prev));
           }
         else if (!strcmp(line, "SHOW_PROGRESS"))
           {
              if (!_progress)
                {
                   efl_event_callback_add
-                     (emo, EFL_CANVAS_VIDEO_EVENT_POSITION_CHANGE, _media_position_update, NULL);
+                     (_emo, EFL_CANVAS_VIDEO_EVENT_POSITION_CHANGE, _media_position_update, NULL);
                   _progress = EINA_TRUE;
                }
           }
         else if (!strcmp(line, "HIDE_PROGRESS"))
           {
              efl_event_callback_del
-                (emo, EFL_CANVAS_VIDEO_EVENT_POSITION_CHANGE, _media_position_update, NULL);
+                (_emo, EFL_CANVAS_VIDEO_EVENT_POSITION_CHANGE, _media_position_update, NULL);
              _progress = EINA_FALSE;
           }
         else if (!strncmp(line, "POSITION", 8))
@@ -123,12 +124,12 @@ _on_stdin(void *data, Ecore_Fd_Handler *fdh EINA_UNUSED)
              if (line[8] == ' ')
                {
                   int pos = atoi(line + 9);
-                  emotion_object_position_set(emo, pos);
+                  emotion_object_position_set(_emo, pos);
                }
              else
                {
-                  int pos = (int)emotion_object_position_get(emo);
-                  int total = (int)emotion_object_play_length_get(emo);
+                  int pos = (int)emotion_object_position_get(_emo);
+                  int total = (int)emotion_object_play_length_get(_emo);
                   printf("POSITION: %d / %d\n", pos, total);
                }
           }
@@ -165,11 +166,11 @@ _on_stdin(void *data, Ecore_Fd_Handler *fdh EINA_UNUSED)
              if (line[6] == ' ')
                {
                   int vol = atoi(line + 7);
-                  emotion_object_audio_volume_set(emo, vol / 100.0);
+                  emotion_object_audio_volume_set(_emo, vol / 100.0);
                }
              else
                {
-                  double vol = emotion_object_audio_volume_get(emo);
+                  double vol = emotion_object_audio_volume_get(_emo);
                   printf("VOLUME: %d\n", (int)(vol * 100));
                }
           }
@@ -195,17 +196,17 @@ int main(int argc, char **argv)
    Eo *e = evas_new();
    evas_output_method_set(e, evas_render_method_lookup("buffer"));
 
-   Eo *emo = emotion_object_add(e);
-   emotion_object_init(emo, NULL);
+   _emo = emotion_object_add(e);
+   emotion_object_init(_emo, NULL);
    efl_event_callback_add
-      (emo, EFL_CANVAS_VIDEO_EVENT_PLAYBACK_STOP, _media_finished, NULL);
+      (_emo, EFL_CANVAS_VIDEO_EVENT_PLAYBACK_STOP, _media_finished, NULL);
 
    if (argc == 2)
      {
-        emotion_object_file_set(emo, argv[1]);
-        emotion_object_play_set(emo, EINA_TRUE);
+        emotion_object_file_set(_emo, argv[1]);
+        emotion_object_play_set(_emo, EINA_TRUE);
      }
-   ecore_main_fd_handler_add(STDIN_FILENO, ECORE_FD_READ, _on_stdin, emo, NULL, NULL);
+   ecore_main_fd_handler_add(STDIN_FILENO, ECORE_FD_READ, _on_stdin, _emo, NULL, NULL);
 
    elm_run();
 
